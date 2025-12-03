37abf2c39df2f52e9a5e52c12f86446d84807cab
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-12-03 23:25:56 +0900

    comment update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,13 +21,18 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
+    # 폼에서 넘어온 데이터(post_params)로 새 게시글 객체를 만듦
     @post = Post.new(post_params)
 
     respond_to do |format|
+      # 저장에 성공하면?
       if @post.save
+        # 방금 쓴 글의 상세 페이지(@post)로 이동
         format.html { redirect_to @post, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
+      # 저장이 실패하면? (제목이 비었거나 등등)
       else
+        # 다시 글쓰기 화면(:new)을 보여줌 (썼던 내용은 그대로 유지)
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -65,6 +70,7 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
+      # 보안 검사: 제목(title)이랑 내용(content)만 받아줌, 해킹 시도 차단
       params.expect(post: [ :title, :content ])
     end
 end
